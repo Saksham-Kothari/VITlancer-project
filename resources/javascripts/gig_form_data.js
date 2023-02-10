c27aa938d08cc37cca42c983f07e7cc99665d247
Mohit Dhaker <mohit.dhaker04@gmail.com>
2023-02-10 23:52:13 +0530

    updt_10-02-23

---
+++ resources/javascripts/gig_form_data.js
@@ -0,0 +1,15 @@
+const firstName = document.getElementById("form6Example1")
+const lastName = document.getElementById("form6Example2")
+const workTittle = document.getElementById("form6Example3")
+const email = document.getElementById("form6Example5")
+const phoneNumber = document.getElementById("form6Example6")
+const upiID = document.getElementById("form6Example4")
+const price = document.getElementById("form6Example10")
+const serviceDescription = document.getElementById("form6Example7")
+const submitButton = document.getElementById("gigFormSubmit")
+// console.log(submitButton)
+
+// login check
+
+
+
